a6746ecc5546ef5da9883e9da7c46ee0ffdc9209
mannamnaveen <mina@naveenmannam.com>
2023-06-11 20:34:20 +0000

    Uploading the complete project

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-let version = 2
+let version = 1
 
 app.get('/', (req, res) => {
   res.send(`This is the version ${version} for CICD.`);
